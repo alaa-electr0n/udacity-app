a34bbf796c48c6280be356241edd837937540cb1
Alaa Elsayed <alaas.m229@gmail.com>
2024-11-05 06:22:14 +0200

    securing my keys

--- website/app.js
+++ website/app.js
@@ -1,7 +1,5 @@
 import updateUI from './scripts/updateUI.mjs';
 
-// const WEATHER_API_KEY = 'b447455f727309cb68eac061b13af2d4';
-
 // const WEATHER_BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
 //https://api.openweathermap.org/data/2.5/weather??zip={zipCode}&appid={api}&units=metric
 
@@ -10,7 +8,7 @@ async function getCountryWeather(postCode) {
 
   try {
     const response = await fetch(
-      `${WEATHER_BASE_URL}?zip=${postCode}&appid=${WEATHER_API_KEY}&units=imperial`
+      `${WEATHER_BASE_URL}?zip=${postCode}&appid=${process.env.WEATHER_API_KEY}&units=imperial`
     );
 
     if (!response.ok) throw new Error('Please Enter A Valid Country Code');
